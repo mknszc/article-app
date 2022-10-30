8742f51ccd6454faf7c5932a1767ececc290d6ce
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2022-10-30 16:29:28 +0300

    edit

--- app/Repositories/CommentRepositories.php
+++ app/Repositories/CommentRepositories.php
@@ -2,7 +2,17 @@
 
 namespace App\Repositories;
 
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+
 class CommentRepositories
 {
-
+	public function create(array $request)
+	{
+		return Comment::create([
+			'author_id' => Auth::user()->getAuthIdentifier(),
+			'article_id' => $request['article_id'],
+			'content' => $request['content'],
+		]);
+	}
 }
