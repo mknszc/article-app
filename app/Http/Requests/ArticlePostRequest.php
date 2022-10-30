d447152b1e1822b7cdfcca4754af081f1d2293e2
Özcan ÇAMOĞLU <ozcan.camoglu@gmail.com>
2022-10-30 16:29:33 +0300

    edit

--- app/Http/Requests/ArticlePostRequest.php
+++ app/Http/Requests/ArticlePostRequest.php
@@ -11,7 +11,7 @@ class ArticlePostRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -21,7 +21,7 @@ class ArticlePostRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
 	        'title' => 'required|string|min:3|max:255',
